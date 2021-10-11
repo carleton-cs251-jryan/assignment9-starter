6368ab80e206a0714085706afa1215395718377d
James Ryan <joryan@ucsc.edu>
2021-10-10 20:43:08 -0500

    Improve instructions

--- vector.h
+++ vector.h
@@ -6,7 +6,7 @@
 struct Vector
 {
     // A pointer to a traditional C array to hold data
-    int* array;
+    int *array;
 
     // The size of the actual C array that you have allocated
     int memorySize;
@@ -23,17 +23,13 @@ typedef struct Vector Vector;
 
 /* Function prototypes. */
 
-/* Take an already existing Vector as a parameter. (Do not create a Vector
- inside this function. Set its internal memory size to match the value given in
- the parameter, allocate an appropriately-sized C array to hold the actual data,
- and initialize the size variable to 0. */
+/* Take an already existing Vector as a parameter. (Do not create a Vector struct
+ inside this function.) Set its internal memory size to match the value given in
+ the parameter, allocate an appropriately sized C array to hold the actual data,
+ initialize the Vector's array variable to point to this array, and initialize 
+ the Vector's size variable to 0. */
 
 void init(Vector *vector, int memorySize);
-
-
-/* Removes the array associated with the Vector. */
-
-void cleanup(Vector *vector);
 
 
 /* Print out the Vector for debugging purposes. */
@@ -44,11 +40,11 @@ void print(Vector *vector);
 /* Insert value at location inside the Vector. If the list has items in
  positions 0 through n-1, you are only permitted to insert new values at
  locations 0 through n. (This is identical to Java's ArrayList behavior.) If
- there is already data at the location you are inserting into, insert slides the
- rest of the data down one position to make room for the new value. Returns 1 if
- success, and 0 if the location is invalid (less than 0, or greater than the
- size). Inserting at the very end of the current data (at position equal to
- size) is fine. All inserts should increase size by 1.
+ there is already data at array[location], insert() slides the rest of the data 
+ down one position to make room for the new value. Return 1 for success, otherwise 0
+ if the location is invalid (less than 0, or greater than the vector size). Inserting 
+ at the very end of the current data (at position equal to size) is fine. All 
+ inserts should increase size by 1.
 
  If the internal array is too small to fit a new value, a new array of twice the
  size is created with malloc. The values are copied, the Vector is appropriately
@@ -57,20 +53,23 @@ void print(Vector *vector);
 int insert(Vector *vector, int location, int value);
 
 
-/* Obtains value at location inside the Vector. Returns 1 if success, and 0 if
- the location is invalid (less than 0, or greater than or equal to the
- size). The value itself is returned through the parameter value. */
+/* Obtains value at location inside the Vector. Return 1 for success, otherwise 
+ 0 if the location is invalid (less than 0, or greater than or equal to the
+ size). The value itself is passed on through the parameter value. */
 
 int get(Vector *vector, int location, int *value);
 
 
-/* Deletes value at location inside the Vector.  Returns 1 if success, and 0 if
- the location is invalid (less than 0, or greater than or equal to the
+/* Removes the array associated with the Vector. */
+
+void cleanup(Vector *vector);
+
+
+/* Deletes value at location inside the Vector.  Return 1 for success, otherwise 
+ 0 if the location is invalid (less than 0, or greater than or equal to the
  size). When an item is deleted, everything else past it in the array should be
  slid down. The internal array itself does not need to be compressed, i.e., you
  don't need to halve it in size if it becomes half full, or anything like
  that. */
 
 int delete(Vector *vector, int location);
-
-
